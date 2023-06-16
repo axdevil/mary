4cc2263d7864acb4d0b02e60b987295072e24144
axdevil <deb.devel@outlook.es>
2023-06-16 03:28:29 -0600

    Formulario de registro

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -4,5 +4,8 @@ const patientModel = require('../models/patientModel')
 module.exports = {
     index:function(req,res){
         res.render('patient/login')
+    },
+    signUp:function(req,res){
+        res.render('patient/signUp')
     }
 }
--- routes/patient.js
+++ routes/patient.js
@@ -5,5 +5,6 @@ const patientController = require('../controllers/patientController')
 
 /* GET home page. */
 router.get('/', patientController.index);
+router.get('/signUp', patientController.signUp)
 
 module.exports = router;

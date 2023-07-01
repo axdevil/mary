6a097bcdebfd561b957636e0d99e238aaae1d45b
axdevil <deb.devel@outlook.es>
2023-07-01 01:19:26 -0600

    Barra de navegacion

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -5,7 +5,7 @@ module.exports = {
         if(!req.session.user){
             res.redirect('/patient')
         }else{
-            res.send('Si hay usuario logeado')
+            res.render('index')
         }
     }
 }
